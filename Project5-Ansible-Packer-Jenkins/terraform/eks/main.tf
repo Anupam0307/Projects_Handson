d56e29bc5a337340f42121c56e3a6400b0df1ff8
Anupam <ghaianupam3@gmail.com>
2026-01-14 16:29:01 +0530

    Updated EKS cluster Public endpoint

--- Project5-Ansible-Packer-Jenkins/terraform/eks/main.tf
+++ Project5-Ansible-Packer-Jenkins/terraform/eks/main.tf
@@ -8,15 +8,19 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
+      desired_size = 1
       min_size     = 1
-      max_size     = 3
-      instance_types = ["t3.small"]
+      max_size     = 1
+      instance_types = ["t3.micro"]
     }
   }
 }
+
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
